865e75948ecc32a87bee44ce29650ee961af8ca9
Ramapuram Naik Mounika <r.n.mounika@gmail.com>
2020-12-18 14:29:49 -0500

    db

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const routes = require("./routes");
 
 // const budgetModel = require('./models/budget_data')
 // const mongoDBClient = require('mongodb').MongoClient
-let url= 'mongodb://localhost:27017/mongodb_budget';
+// let url= 'mongodb://localhost:27017/mongodb_budget';
+let url= 'mongodb+srv://mounika:mounika@cluster0.wx8cs.mongodb.net/mongodb_budget?retryWrites=true&w=majority';
+// mongodb+srv://mounika:<password>@cluster0.wx8cs.mongodb.net/<dbname>?retryWrites=true&w=majority
 
 mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology:true})
 .then(()=>{
